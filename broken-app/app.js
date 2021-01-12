839bf9637ca91259bc2ccf560ea3479d7b827991
Sandra Spanikova <spanikovasandra@gmail.com>
2021-01-12 10:52:58 +0100

    fix broken app.js

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,18 +1,46 @@
-const express = require('express');
-let axios = require('axios');
-var app = express();
+const express = require("express");
+const axios = require("axios");
+const ExpressError = require("./expressError");
 
-app.post('/', function(req, res, next) {
+const app = express();
+app.use(express.json());
+
+app.post("/", function (req, res, next) {
   try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
+    const promiseArr = req.body.developers.map((username) => {
+      return axios.get(`https://api.github.com/users/${username}`);
     });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
-
-    return res.send(JSON.stringify(out));
+    Promise.all(promiseArr).then((results) => {
+      const userInfo = results.map((r) => ({
+        name: r.data.name,
+        bio: r.data.bio,
+      }));
+      return res.json({ data: userInfo });
+    });
   } catch {
     next(err);
   }
 });
 
-app.listen(3000);
+// 404 handler
+app.use(function (req, res, next) {
+  return next(new ExpressError("Not Found", 404));
+});
+
+// generic error handler
+app.use(function (err, req, res, next) {
+  // the default status is 500 Internal Server Error
+  let status = err.status || 500;
+
+  // set the status and alert the user
+  return res.status(status).json({
+    error: {
+      message: err.message,
+      status: status,
+    },
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server is listening on port 3000");
+});
--- broken-app/issues.md
+++ broken-app/issues.md
@@ -1,1 +1,9 @@
 # Broken App Issues
+
+1. rewrote let and var into const
+2. inserted print statement into app.listen
+3. added error handling
+4. added middleware to parse JSON
+5. rewrote async callback inside map - using promises and promise.all instead
+6. renamed unclear variables such as "d" and "out"
+7. returning res.json instead of res.send
